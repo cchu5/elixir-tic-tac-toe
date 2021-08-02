1d052d3c68009e54f05f458af7e775a4112255c0
Chris <cchu@8thlight.com>
2021-08-02 11:20:54 -0700

    Add assertion that board should have 9 squares

--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -5,10 +5,14 @@ defmodule TicTacToeTest do
   test "create new board" do
     assert function_exported?(TicTacToe, :new_board, 0) == true
     assert TicTacToe.new_board() |> is_map == true
-    assert TicTacToe.new_board() |> Map.keys |> length > 0
 
     board = TicTacToe.new_board()
     expected_square = %Square{position: 1}
     assert board |> Enum.fetch(0) == {:ok, {expected_square, :empty}}
+  end
+
+  test "board should have 9 squares" do
+    assert TicTacToe.new_board() |> Map.keys |> length > 0
+    assert TicTacToe.new_board() |> Map.keys |> Enum.count == 9
   end
 end
