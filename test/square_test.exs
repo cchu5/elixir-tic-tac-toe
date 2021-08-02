6c2d4f362df139554ed38bc0a277d9bd0226df58
Chris <cchu@8thlight.com>
2021-08-02 11:06:14 -0700

    Only allow squares between 1 to 9

--- test/square_test.exs
+++ test/square_test.exs
@@ -4,6 +4,11 @@ defmodule SquareTest do
 
   test "create a square" do
     assert function_exported?(Square, :new, 1) == true
+  end
+
+  test "square positions should only be 1..9" do
     assert Square.new(1) == {:ok, %Square{position: 1}}
+    assert Square.new(9) == {:ok, %Square{position: 9}}
+    assert Square.new(10) == {:error, :invalid_square}
   end
 end
