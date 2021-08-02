ef6ff59ab4d0f5821663b377134ab763fbef4631
Chris <cchu@8thlight.com>
2021-08-02 11:26:31 -0700

    Create function for check_player

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -1,4 +1,9 @@
 defmodule TicTacToe do
+
+  def check_player(player) do
+    "player #{player}"
+  end
+  
   def new_board do
     squares = for position <- 1..9, into: MapSet.new(), do: %Square{position: position}
     for square <- squares, into: %{}, do: {square, :empty}
