671025612f879c770e85dd4379b06b49ce9a7991
Chris <cchu@8thlight.com>
2021-08-02 09:52:33 -0700

    Update new board test to expect a map

--- test/tic_tac_toe_test.exs
+++ test/tic_tac_toe_test.exs
@@ -3,6 +3,6 @@ defmodule TicTacToeTest do
   doctest TicTacToe
 
   test "create new board" do
-    assert function_exported?(TicTacToe, :new_board, 0) == true
+    assert TicTacToe.new_board() |> is_map == true
   end
 end
